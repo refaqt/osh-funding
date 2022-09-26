1779d51fa579a696fe67c4c817cd676a92c72251
thinklerb <niels.bosmans@refaqt.com>
2022-09-26 16:25:37 +0200

    simulation of the expected supply and reserve for funding platform

--- abcsim.py
+++ abcsim.py
@@ -8,12 +8,13 @@ from matplotlib.pyplot import plot, figure, show, xlabel, ylabel
 t = np.linspace(0, 4 * 52, 1000)  # (weeks) time
 i = 1  # annual growth rate of the platform
 F0 = 10000  # (EUR) amount of funding allocated in the first week of the platform
-d0 = 100000  # (EUR) initial raise
+d0 = 200000  # (EUR) initial raise
 theta = 0.5  # initial allocation to funding pool
 p0 = 0.1  # (DAI) initial price per token
-p1 = 0.15  # (DAI) post-hatch price per token
+p1 = 0.25  # (DAI) post-hatch price per token
 rF = 0.05  # (-) percentage fee when funding projects
 rS = 0.05  # (-) percentage fee when selling tokens
+rT = 0.1  # (-) percentage of funding that is used to create tokens for the donor
 
 # Variables
 kappa = p1 / (p0 * (1 - theta))  # power in power function invariant V(R,S)
@@ -25,23 +26,76 @@ V0 = S0 ** kappa / R0  # initial value of the invariant function
 Slin = np.linspace(0, 5e6, 1000)  # S linear variation of token supply
 PSlin = kappa * Slin ** (kappa - 1) / V0  # (EUR) P(S) token price for linear variation of the token supply
 Rlin = Slin ** kappa / V0  # (EUR) R(S) reserve for linear variation of the token supply
-d = theta * d0 + np.cumsum(F * rF)  # (EUR) size of funding pool
+d = theta * d0 + np.cumsum(F * rF)  # (EUR) size of Refaqt's funding pool
+
+# Simulation
+Fp = np.cumsum(F * (1 - rF) * (1 - rT))  # Total amount of funding to projects
+S_m = np.zeros((len(F), len(F)))  # Number of tokens, split up per week
+DeltaS = np.zeros((len(F), len(F)))  # Change of the supply each week
+R_m = np.zeros((len(F), len(F)))  # Part of the reserve attributed to the tokens minted each week
+DeltaR = np.zeros((len(F), len(F)))  # Change of the reserve each week
+S = np.zeros(len(F))
+S[0] = S0
+R = np.zeros(len(F))
+R[0] = R0
+P = np.zeros(len(F))
+P[0] = p1
+j = 0
 
 
-figure("Amount of funding")
+for i in range(1, len(F)):
+    DeltaR[i, i] = F[i-1] * (1 - rF) * rT
+    # Insert change in reserve from burning of tokens here
+    R[i] = R[i-1] + np.sum(DeltaR[i, :])
+    DeltaS[i, i] = (V0 * R[i]) ** (1 / kappa) - S[i-1]
+    S[i] = S[i-1] + np.sum(DeltaS[i, i])
+    P[i] = kappa * R[i] ** ((kappa - 1) / kappa) / V0 ** (1 / kappa)
+
+
+figure("Weekly amount of funding")
 plot(t, F)
 xlabel("time (weeks)")
-ylabel("Funding (EUR)")
+ylabel("Funding / week (EUR)")
 
 figure("Token price")
 plot(Slin, PSlin)
 xlabel("Token supply (number of tokens)")
 ylabel("Token price (EUR)")
 
-figure("Funding pool")
+figure("Funding pool Refaqt")
 plot(t, d)
 xlabel("time (weeks)")
-ylabel("Funding pool (EUR)")
+ylabel("Funding (EUR)")
+
+figure("Projected yearly revenue Refaqt")
+plot(t, F * rF * 52)
+xlabel("time (weeks)")
+ylabel("Revenue (EUR)")
+
+figure("DeltaS")
+plot(t, [DeltaS[i, i] for i in range(len(F))])
+xlabel("time (weeks)")
+ylabel("\Delta S (-)")
+
+figure("Reserve / weekly")
+plot(t, R)
+xlabel("time (weeks)")
+ylabel("Reserve (EUR)")
+
+figure("Supply / weekly")
+plot(t, S)
+xlabel("time (weeks)")
+ylabel("Supply (-)")
+
+figure("Token price / weekly")
+plot(t, P)
+xlabel("time (weeks)")
+ylabel("Token price (EUR)")
+
+figure("Total funding to projects")
+plot(t, Fp)
+xlabel("time (weeks)")
+ylabel("Funding (EUR)")
 
 # figure("Reserve / Supply")
 # plot(Slin, Rlin)
